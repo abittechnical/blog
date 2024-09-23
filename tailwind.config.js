b95c792b0635639a4123288d430bc29a8d4eecc4
George Bockari <bitwhys@gmail.com>
2024-09-23 00:52:13 -0400

    feat(colors): add light and dark theme gray colors from vercel design-system (geist)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      backgroundColor: {
+        1: 'var(--whys-background-default)',
+        2: 'var(--whys-background-secondary)',
+        gray: {
+          DEFAULT: 'var(--whys-surface-soft-gray-default)',
+          hover: 'var(--whys-surface-soft-gray-hover)',
+          active: 'var(--whys-surface-soft-gray-active)',
+        },
+        solid: {
+          DEFAULT: 'var(--whys-surface-solid-gray-default)',
+          hover: 'var(--whys-surface-solid-gray-hover)',
+        },
+      },
+      borderColor: {
+        DEFAULT: 'var(--whys-border-gray-default)',
+        hover: 'var(--whys-border-gray-hover)',
+        active: 'var(--whys-border-gray-active)',
+      },
+      ringColor: {
+        DEFAULT: 'var(--whys-border-gray-default)',
+        hover: 'var(--whys-border-gray-hover)',
+        active: 'var(--whys-border-gray-active)',
+      },
+      textColor: {
+        primary: 'var(--whys-text-primary-gray)',
+        secondary: 'var(--whys-text-secondary-gray)',
+      },
+    },
   },
   plugins: [],
 };
